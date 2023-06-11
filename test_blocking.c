4888bbfddece0305ff57af3ab1f803d37c39df1a
Francesco Pio Montrano <francesco.montrano@alten.it>
2023-06-11 21:38:26 +0200

    non_blocking YES blocking NO unit test TO_TEST

--- test_blocking.c
+++ test_blocking.c
@@ -63,16 +63,25 @@ void test_scenario_1() {
 
     pthread_t producer_t;
     pthread_create(&producer_t, NULL, blocking_producer, buffer);
-    CU_ASSERT_EQUAL(buffer->count, 0);
+
+    pthread_join(producer_t, NULL);
+    CU_ASSERT_EQUAL(buffer->count, 1);
 
     buffer_destroy(buffer);
 }
 
 void test_scenario_2() {
     buffer_t *buffer = buffer_init(1);
+    CU_ASSERT_EQUAL(buffer->count, 0)
+
+    buffer->messages[buffer->count] = msg_init("val");
+    buffer->count++;
+    CU_ASSERT_EQUAL(buffer->count, 1);
 
     pthread_t consumer_t;
     pthread_create(&consumer_t, NULL, blocking_consumer, buffer);
+
+    pthread_join(consumer_t, NULL);
     CU_ASSERT_EQUAL(buffer->count, 0);
 
     buffer_destroy(buffer);
@@ -87,6 +96,9 @@ void test_scenario_3() {
     pthread_create(&producer_t, NULL, blocking_producer, buffer);
     pthread_create(&consumer_t, NULL, blocking_consumer, buffer);
 
+    pthread_join(consumer_t, NULL);
+    pthread_join(producer_t, NULL);
+
     buffer_destroy(buffer);
 }
 
@@ -98,6 +110,9 @@ void test_scenario_4() {
 
     pthread_create(&producer_t_1, NULL, blocking_producer, buffer);
     pthread_create(&producer_t_2, NULL, blocking_producer, buffer);
+
+    pthread_join(producer_t_1, NULL);
+    pthread_join(producer_t_2, NULL);
 
     buffer_destroy(buffer);
 }
@@ -111,6 +126,9 @@ void test_scenario_5() {
     pthread_create(&consumer_t_1, NULL, blocking_consumer, buffer);
     pthread_create(&consumer_t_2, NULL, blocking_consumer, buffer);
 
+    pthread_join(consumer_t_1, NULL);
+    pthread_join(consumer_t_2, NULL);
+
     buffer_destroy(buffer);
 }
 
@@ -122,6 +140,9 @@ void test_scenario_6() {
 
     pthread_create(&producer_t_1, NULL, blocking_producer, buffer);
     pthread_create(&producer_t_2, NULL, blocking_producer, buffer);
+
+    pthread_join(producer_t_1, NULL);
+    pthread_join(producer_t_2, NULL);
 
     buffer_destroy(buffer);
 }
@@ -138,6 +159,11 @@ void test_scenario_7() {
     pthread_create(&producer_t_2, NULL, blocking_producer, buffer);
     pthread_create(&producer_t_3, NULL, blocking_producer, buffer);
     pthread_create(&producer_t_4, NULL, blocking_producer, buffer);
+
+    pthread_join(producer_t_1, NULL);
+    pthread_join(producer_t_2, NULL);
+    pthread_join(producer_t_3, NULL);
+    pthread_join(producer_t_4, NULL);
 
     buffer_destroy(buffer);
 }
@@ -157,6 +183,12 @@ void test_scenario_8() {
     pthread_create(&producer_t_4, NULL, blocking_producer, buffer);
     pthread_create(&producer_t_5, NULL, blocking_producer, buffer);
 
+    pthread_join(producer_t_1, NULL);
+    pthread_join(producer_t_2, NULL);
+    pthread_join(producer_t_3, NULL);
+    pthread_join(producer_t_4, NULL);
+    pthread_join(producer_t_5, NULL);
+
     buffer_destroy(buffer);
 }
 
@@ -168,6 +200,9 @@ void test_scenario_9() {
 
     pthread_create(&consumer_t_1, NULL, blocking_consumer, buffer);
     pthread_create(&consumer_t_2, NULL, blocking_consumer, buffer);
+
+    pthread_join(consumer_t_1, NULL);
+    pthread_join(consumer_t_2, NULL);
 
     buffer_destroy(buffer);
 }
@@ -181,9 +216,14 @@ void test_scenario_10() {
     pthread_t consumer_t_2;
 
     pthread_create(&producer_t_1, NULL, blocking_producer, buffer);
-    pthread_create(&producer_t_2, NULL, blocking_producer, buffer);
     pthread_create(&consumer_t_1, NULL, blocking_consumer, buffer);
+    pthread_create(&producer_t_2, NULL, blocking_producer, buffer);
     pthread_create(&consumer_t_2, NULL, blocking_consumer, buffer);
+
+    pthread_join(producer_t_1, NULL);
+    pthread_join(consumer_t_1, NULL);
+    pthread_join(producer_t_2, NULL);
+    pthread_join(consumer_t_2, NULL);
 
     buffer_destroy(buffer);
 }
@@ -200,6 +240,11 @@ void test_scenario_11() {
     pthread_create(&producer_t_2, NULL, blocking_producer, buffer);
     pthread_create(&consumer_t_1, NULL, blocking_consumer, buffer);
     pthread_create(&consumer_t_2, NULL, blocking_consumer, buffer);
+
+    pthread_join(producer_t_1, NULL);
+    pthread_join(consumer_t_1, NULL);
+    pthread_join(producer_t_2, NULL);
+    pthread_join(consumer_t_2, NULL);
 
     buffer_destroy(buffer);
 }
